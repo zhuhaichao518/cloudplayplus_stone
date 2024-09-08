69d5702780683513ac1fb821fa4c88f851f00650
Haichao Zhu <zhuhaichao518@gmail.com>
2024-09-08 19:22:51 +0800

    Add Vitrtual Keyboard to plugin.

---
+++ lib/global_settings/keyboard_settings.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class KeyboardSettings {
+  static Color keyBackgroundColor = Colors.grey;
+  static Color keyFontColor = Colors.black;
+  static int keyWidth = 10;
+  static int keyHeight = 10;
+  static void initialize(){
+    
+  }
+}
