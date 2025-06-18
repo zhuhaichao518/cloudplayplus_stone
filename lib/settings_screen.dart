bee7a39e09aaf0d2b314947914e114d262d1305e
zhuhaichao518 <zhuhaichao518@gmail.com>
2025-06-18 09:42:25 +0800

    bump to 1.0.6

--- lib/settings_screen.dart
+++ lib/settings_screen.dart
@@ -319,7 +319,7 @@ class _SettingsScreenState extends State<SettingsScreen> {
           ),
           SettingsSection(title: const Text('版本号'), tiles: <SettingsTile>[
             SettingsTile(
-              title: const Text('1.0.5'),
+              title: const Text('1.0.6'),
               leading: const Icon(Icons.sunny),
             ),
             if (AppPlatform.isMobile)
